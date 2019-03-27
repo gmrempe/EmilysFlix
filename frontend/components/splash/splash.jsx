8745384b0a5c681a562d2a9fe22b552cc71c083d
greg <gmrempe@gmail.com>
2019-03-27 12:12:42 -0400

    Complete Splash Styling

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -19,13 +19,17 @@ class Splash extends React.Component {
     render() {
         return (
             <div className="landing">
+                {/* <img className='logo' src="/assets/logo.png"/> */}
                 <Link className="signin-button" to="/login">Sign In</Link>
-                <div className="landing-elements">
-                    <h1 >See what's next.</h1>
-                    <h2 >WATCH ANYWHERE. CANCEL ANYTIME.</h2>
-            
-                    <Link className="signup-button" to="/signup">TRY 30 DAYS FREE</Link>
-                    <button onClick={this.handleLogin}>Demo Sign In</button>
+                <div className="landing-content">
+                        <p>EMILYSFLIX</p>
+                    <div className="landing-elements">
+                        <h1 >See what's next.</h1>
+                        <h2 >WATCH ANYWHERE. CANCEL ANYTIME.</h2>
+                
+                        <Link className="signup-button" to="/signup">TRY 30 DAYS FREE</Link>
+                        <button onClick={this.handleLogin}>Demo Sign In</button>
+                    </div>
                 </div>
             </div>
         )
