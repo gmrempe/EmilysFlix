0096b4c14b3817e7acfe3e496fce3952a6de1729
greg <gmrempe@gmail.com>
2019-04-03 11:42:28 -0400

    Correct seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # require "open-uri"
+VideoGenre.destroy_all
+Genre.destroy_all
+Video.destroy_all
+User.destroy_all
+
 
 User.create(email: "Saitama@gmail.com", password: "#OnePunch")
 
